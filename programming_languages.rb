2405d52a25b9bf618efaeb77489c01e1fae53ddf
Sean Hetzel <seanhetzel1@gmail.com>
2019-05-28 20:12:38 +0000

    Automatically backed up by Learn

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,11 @@
 def reformat_languages(languages)
   # your code here
+  new_hash = {}
+  languages.each do |oo_or_func, language|
+    language.each do |language_name, language_attributes|
+      
+      new_hash[language_name] = {:type => languages[oo_or_func][language_name][:type], :style => [oo_or_func]}
+    end
+  end
+  new_hash
 end
